a3a134a4f46696473fa238ef8f1beed363e53cac
ddebring <32481548+ddebring@users.noreply.github.com>
2021-06-08 15:18:26 +0530

    Add files via upload

---
+++ fb.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyBIOlUp208uP8_52TB-4n-UBEsleQOPU6Y",
+    authDomain: "social-38bb5.firebaseapp.com",
+    projectId: "social-38bb5",
+    storageBucket: "social-38bb5.appspot.com",
+    messagingSenderId: "516182233273",
+    appId: "1:516182233273:web:719e0c8bb7ab7ef7f70b68",
+    measurementId: "G-W49CXYV5E6"
+  };
+
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+export default firebase;
